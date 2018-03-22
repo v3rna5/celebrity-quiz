cead03936dca22a78700441bc418380d6b463d78
Dennise Ortega and Verna Santos <dennise@>
2018-03-22 16:15:40 -0700

    add more if statements

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,19 @@ $(document).ready(function() {
     console.log("I AM THE QUIZ");
     var gender1 = $("select#gender").val();
     console.log(gender1);
+    var food = $("select#food1").val();
+    var color = $("select#color1").val();
 
-
-    if (gender1 === 'male') {
+    if (gender1 === 'other' && food === 'pizza' && color === 'blue') {
         $(".celebrity #tomhardy").show();
       }
+
+    if (gender1 === 'male' && food === 'burgers' && color === 'purple') {
+       $(".celebrity #jennifer").show();
+     }
+
+
+
 
     event.preventDefault();
   });
